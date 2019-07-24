82f9b429db1fac16533d945cb0136931e0413a16
giovanni_machine_learning <gioml@BO1LCM1038.telecomitalia.local>
2019-07-24 09:40:28 +0200

    threshold

--- vault/example/main.go
+++ vault/example/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"flag"
 	"github.com/axamon/tokenizzatore/vault"
 )
 
+var numkey = flag.Int("nk",3,"Numero di chiavi superAdmin da creare")
+var threashhold = flag.Int("nm",2,"Numero minimo di chiavi per sbloccare")
+
 func main() {
-	vault.CreaChiaviSuperAdmin(3,2)
+
+	flag.Parse()
+	vault.CreaChiaviSuperAdmin(*numkey,*threashhold)
 }
